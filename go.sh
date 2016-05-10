59597e3c3fc7ef578c15316ce1da56a9304266c2
12345678aaa <1063988762@qq.com>
2016-05-10 20:21:30 +0800

    添加go.sh

--- go.sh
+++ go.sh
@@ -1,6 +1,6 @@
 git config --global user.email "1063988762@qq.com"
 git config --global user.name "12345678aaa"
-git config --global push.defauit simple
+git config --global push.default simple
 git add .
 git commit -m $1
 git push
